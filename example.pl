01bb57d254b505073d681205ccc45f3b61f0bba2
Kyle Dawkins <kdawkins@fastmail.fm>
2011-12-30 03:40:31 +1300

    Added a version of the example that the guys use.

---
+++ example.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/env perl
+
+use OneTimeSecret;
+
+# Note: replace these with yours in order for this to work!
+my $customerId  = 'chris@onetimesecret.com';
+my $testApiKey  = '4dc74a03fwr9aya5qur5wa8vavo4gih1hasj6181';
+
+my $api = OneTimeSecret->new( $customerId, $testApiKey );
+my $result = $api->shareSecret( 'Jazz, jazz and more jazz.',
+                   password => 'thepassword',
+                   recipient => 'kyle@shoffle.com',
+                   ttl => 7200,
+                 );
+printf( "%s\n", $result->{secret_key} );
